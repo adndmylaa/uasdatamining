1cdc49c8de42c178a91ac9793be2c3f1f92b2770
Adinda Meylia S <113561771+adndmylaa@users.noreply.github.com>
2025-07-03 06:25:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def index():
 
 # Jalankan app Flask dalam mode debug
 if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
